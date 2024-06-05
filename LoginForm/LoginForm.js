746694db0ade67e59c7c61438339f5c1243bf897
Yusuf Placanica <yplacani@emeal.nttdata.com>
2024-06-05 18:04:26 +0200

    Login Form Submit

---
+++ LoginForm/LoginForm.js
@@ -0,0 +1,23 @@
+const form = document.querySelector("form");
+
+const onSubmit = (event) => {
+  event.preventDefault();
+  const email = document.querySelector('input[type="email"]').value;
+  const password = document.querySelector('input[type="password"]').value;
+  console.log(email);
+  console.log(password);
+  fetch("http://localhost:3000", {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+  });
+};
+
+//form.onsubmit = onSubmit;
+form.addEventListener("submit", onSubmit);
+
+//Gestione Link Google
+const linkGoogle = document.querySelector("a");
+linkGoogle.onclick = (event) => {
+  event.preventDefault();
+  window.alert("Ti Avevo Detto Di Non Cliccare!");
+};
